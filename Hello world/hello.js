eead10eb1b3e0e11012fe69a2c3dc26653364e01
msalmanzafar <m.salman.zafar292@gmail.com>
2017-07-06 00:14:32 -0700

    updates lerning node.js

--- Hello world/hello.js
+++ Hello world/hello.js
@@ -1,12 +1,16 @@
-console.log('Hello Sky');
+// console.log('Hello Sky');
 
-var Hello = 'this is new sky here';
+// var Hello = 'this is new sky here';
 
-console.log(Hello);
+// console.log(Hello);
 
-var copy = Hello.slice(12);
+// var copy = Hello.slice(12);
 
-console.log(`oh yes ${copy}`);
+// console.log(`oh yes ${copy}`);
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
+
+var path = require('path');
+
+console.log(`am working on ${path.basename(__filename)}`);
