462e252137d9d8bcfdbfa5a5594322270cccaf8e
msalmanzafar <m.salman.zafar292@gmail.com>
2017-07-05 23:21:08 -0700

    Understaning

---
+++ Hello world/hello.js
@@ -0,0 +1,12 @@
+console.log('Hello Sky');
+
+var Hello = 'this is new sky here';
+
+console.log(Hello);
+
+var copy = Hello.slice(12);
+
+console.log(`oh yes ${copy}`);
+
+console.log(__dirname);
+console.log(__filename);
